19132cdfd68b674c4f3be13500b205d89cf58638
Michael Nov <m93.autumn@gmail.com>
2016-06-03 15:25:56 +0200

    db changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20160602210405) do
 
-  create_table "simple_captcha_data", force: :cascade do |t|
-    t.string   "key",        limit: 40
-    t.string   "value",      limit: 6
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "simple_captcha_data", ["key"], name: "idx_key"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -41,7 +35,7 @@ ActiveRecord::Schema.define(version: 20160602210405) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
